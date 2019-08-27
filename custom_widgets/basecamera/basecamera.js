9aac227fcb50156e7d4cbb5c8d7bdcd78dd86182
Daniel Lashua <daniel@inklog.net>
2019-08-27 14:01:19 -0500

    first commit

---
+++ custom_widgets/basecamera/basecamera.js
@@ -0,0 +1,84 @@
+function basecamera(widget_id, url, skin, parameters)
+{
+    self = this
+
+    console.log(parameters)
+
+    // Initialization
+    
+    self.parameters = parameters;
+    
+    var callbacks = []
+
+    self.OnStateAvailable = OnStateAvailable
+    self.OnStateUpdate = OnStateUpdate
+
+    var monitored_entities = 
+        [
+            {"entity": parameters.entity, "initial": self.OnStateAvailable, "update": self.OnStateUpdate},
+        ];
+    
+    // Call the parent constructor to get things moving
+    
+    WidgetBase.call(self, widget_id, url, skin, parameters, monitored_entities, callbacks);
+
+    // Set the url
+    
+    self.index = 0;
+    refresh_frame(self)
+    
+    
+    function refresh_frame(self)
+    {
+        console.log(self)
+
+        if ("base_uri" in self.parameters && "entity_picture" in self) {
+            var url = self.parameters.base_uri + self.entity_picture
+        } 
+        else 
+        {
+            var url = '/images/Blank.gif'
+        }
+
+        if (url.indexOf('?') > -1)
+        {
+            url = url + "&time=" + Math.floor((new Date).getTime()/1000);
+        }
+        else
+        {
+            url = url + "?time=" + Math.floor((new Date).getTime()/1000);
+        }
+        self.set_field(self, "img_src", url);
+        self.index = 0
+        console.log(url)
+        if ("refresh" in self.parameters)
+        {
+            setTimeout(function() {refresh_frame(self)}, self.parameters.refresh * 1000);
+        }
+    }
+
+    // Function Definitions
+    
+    // The StateAvailable function will be called when 
+    // self.state[<entity>] has valid information for the requested entity
+    // state is the initial state
+    
+    function OnStateAvailable(self, state)
+    {        
+        self.state = state.state;
+        self.entity_picture = state.attributes.entity_picture
+        refresh_frame(self)
+    }
+    
+    // The OnStateUpdate function will be called when the specific entity
+    // receives a state update - its new values will be available
+    // in self.state[<entity>] and returned in the state parameter
+    
+    function OnStateUpdate(self, state)
+    {
+        self.state = state.state;
+        self.entity_picture = state.attributes.entity_picture
+        refresh_frame(self)
+    }
+
+}
